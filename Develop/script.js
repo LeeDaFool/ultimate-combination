a77cef22ceb2a12b39132b944b4df2b77f7c60cb
Chang Metellus <metellus.chang@gamil.com>
2022-10-20 20:06:03 -0400

    implemented all the criteria

--- Develop/script.js
+++ Develop/script.js
@@ -8,11 +8,34 @@ var fullChars = {
   charsetNum: "0123456789",
   charsetSym: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
 };
+var charactersCriteria = function() {
+  var secondCriteria = confirm("Include lowercase?");
+  var thirdCriteria = confirm("Include uppercase?");
+  var fourthCriteria = confirm("Include number?");
+  var lastCriteria = confirm("Include special characters?");
+}
+
 document.querySelector('#generate').addEventListener('click', userChoice);
 
 function userChoice() {
-    }
-
+    var firstCriteria = prompt("Please choose the length of your password between 8-128:");
+    if(firstCriteria < 8 || firstCriteria > 128) {
+      alert("Try again");
+      userChoice();
+    } else {
+      passLength = firstCriteria;
+      console.log(passLength);
+      charactersCriteria();
+      
+      }
+    };
+    
+    
+    // for(var i = 0; i < firstCriteria; i++) {
+  // do {
+  //   alert("Try again");
+  //   confirm("Include lowercase?")
+  // } while (secondCriteria === false);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
